323e4a549c22c14e7462328976885441d4fd1e23
joaovictorpachecofoskersul-cmd <joaovictorpachecofoskersul@gmail.com>
2026-03-30 02:16:02 -0300

    Update db.js

--- db.js
+++ db.js
@@ -2,9 +2,9 @@ const mysql = require("mysql2");
 
 const db = mysql.createPool({
   host: "auth-db1601.hstgr.io",
-  user: "u51961382_fluxo_admin",
-  password: "A123456Senha@",
-  database: "u51961382_fluxo_caixa_si",
+  user: "u519611382_testefinal",
+  password: "21Joaovictor21",
+  database: "u519611382_testefinal",
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
@@ -14,7 +14,8 @@ db.getConnection((err, connection) => {
   if (err) {
     console.error("❌ ERRO:", err.message);
   } else {
-    console.log("✅ CONECTADO!");
+    console.log("✅ CONECTADO COM SUCESSO!");
+    console.log("📊 Banco: u519611382_testefinal");
     connection.release();
   }
 });
